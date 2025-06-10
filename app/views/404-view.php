1e781b78462c5795d8aa7d4346495cb6299292db
Pablo Escalante <pabloescalante200027@gmail.com>
2025-06-10 13:28:07 -0500

    first commit

---
+++ app/views/404-view.php
@@ -0,0 +1,4 @@
+<div class="flex justify-center items-center w-full h-dvh gap-5 flex-col">
+    <h1 class="text-3xl font-bold">ERROR 404</h1>
+    <p class="text-xl">Page not found</p>
+</div>
---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+    if(isset($_GET['views'])){
+        $url=explode("/", $_GET['views']);
+    }else{
+        $url=["home"];
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    <title>Personal Blog</title>
+</head>
+<body>
+    <?php
+    
+    $vista = $url[0];
+
+    if($vista === "home" || $vista === "article"){
+        require_once(__DIR__."/app/views/".$vista."-view.php");
+    } else if($vista === "admin" || $vista === "edit" || $vista === "new") {
+        require_once(__DIR__."/app/views/admin/".$vista."-view.php");
+    } else {
+        require_once(__DIR__."/app/views/404-view.php");
+    }
+
+    ?>
+</body>
+</html>
